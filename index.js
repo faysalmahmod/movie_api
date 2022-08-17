713ddebce058275e1a1f38e6f080346a95a10c70
Faisal mahmood <faysalmahmod7@gmail.com>
2022-08-17 03:18:16 +0200

    Allow access to all Origins

--- index.js
+++ index.js
@@ -18,22 +18,23 @@ app.use(bodyParser.json());
 let auth = require('./auth.js')(app);
 const passport= require('passport');
 require('./passport.js');
-let cors = require('cors');
+const cors = require('cors');
+app.use(cors());
 //app.use(cors()); // Allow all domains to access APi
 
 ///List of allowed domains to access API//
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-app.use(cors({
-  origin : (origin,callback) => {
-    if(!origin) return callback(null,true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      let messsage = "The CORS policy for this application doesn’t allow access from origin " + origin;
-      return callback(new Error(messsage),false);
-    }
-    return callback(null,true);
-  }
-}));
+// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+// app.use(cors({
+//   origin : (origin,callback) => {
+//     if(!origin) return callback(null,true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       let messsage = "The CORS policy for this application doesn’t allow access from origin " + origin;
+//       return callback(new Error(messsage),false);
+//     }
+//     return callback(null,true);
+//   }
+// }));
 // /*
 
 ///////////////////////////////////////////////////GET(Read) Queries///////////////////
