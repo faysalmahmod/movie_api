18da5e416a85731e86fa70d2bc9fd0790d1c5746
Faisal mahmood <faysalmahmod7@gmail.com>
2022-09-19 05:57:45 +0200

    Authentication removed for testing purpose

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/',(req,res) => {
 })
 //Read movies using authentication
 
-app.get('/movies',passport.authenticate('jwt',{session:false}),(req,res) => {
+app.get('/movies',(req,res) => {
    Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
